bb8ffa00d4ee05f200c98fa22eb88fab8d96da39
guanzhong <catyguan@gmail.com>
2013-07-28 15:30:20 +0800

    finish sqlite crypt

--- gservice/sqlite/sqlite3encrypt.c
+++ gservice/sqlite/sqlite3encrypt.c
@@ -3,13 +3,7 @@
 #include "sqlite3.h"
 #include "blowfish.h"
 
-#ifndef DB_KEY_LENGTH_BYTE         /*密钥长度*/
-#define DB_KEY_LENGTH_BYTE   16   /*密钥长度*/
-#endif
- 
-#ifndef DB_KEY_PADDING             /*密钥位数不足时补充的字符*/
-#define DB_KEY_PADDING       0x33 /*密钥位数不足时补充的字符*/
-#endif
+#define BLOWFISH_MODE	0
 
 void* ext_createCodec(const void *pKey, int nKeyLen)
 {
@@ -32,23 +26,27 @@ void* ext_createCodec(const void *pKey, int nKeyLen)
 	return codec;
 }
 
-// #define CONFUSION
+#define CONFUSION
 
 int ext_encrypt(void * pData, unsigned int data_len, void* codec)
 {
 	Blowfish* bf = (Blowfish*) codec;
+	Encrypt(bf, (unsigned char*) pData, data_len, (unsigned char*) pData, data_len, BLOWFISH_MODE);
 #ifdef CONFUSION
-	unsigned char* p = (unsigned char*) pData;
-	unsigned int i;
-	unsigned char val;
-	for (i = 0; i < data_len; i++)
-	{
-		val = ~(*p);
-		*p = val;
-		p++;
+	if(1) {
+		unsigned char* p = (unsigned char*) pData;
+		unsigned int i;
+		unsigned char val;
+		for (i = 0; i < data_len; i++)
+		{
+			val = *p;
+			if(val!=0 && val!=0xFF) {
+				*p = ~val;
+			}
+			p++;
+		}
 	}
-#endif
-	Encrypt(bf, (unsigned char*) pData, data_len, (unsigned char*) pData, data_len, 0);
+#endif	
 	return 0;
 }
 
@@ -61,11 +59,13 @@ int ext_decrypt(void * pData, unsigned int data_len, void* codec)
 	unsigned char val;
 	for (i = 0; i < data_len; i++)
 	{
-		val = ~(*p);
-		*p = val;
+		val = *p;
+		if(val!=0 && val!=0xFF) {
+			*p = ~val;
+		}
 		p++;
 	}
 #endif
-	Decrypt(bf, (unsigned char*) pData, data_len, (unsigned char*) pData, data_len, 0);
+	Decrypt(bf, (unsigned char*) pData, data_len, (unsigned char*) pData, data_len, BLOWFISH_MODE);
 	return 0;
 }
