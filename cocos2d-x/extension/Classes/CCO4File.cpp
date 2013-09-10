73b7e6c313ba553b0f860c4fe543b55a8c981e48
guan zhong <catyguan@gmail.com>
2013-09-10 15:35:08 +0800

    CCO4File to FileSystem

--- cocos2d-x/extension/Classes/CCO4File.cpp
+++ cocos2d-x/extension/Classes/CCO4File.cpp
@@ -4,6 +4,7 @@
 #include "cocoa/CCValueSupport.h"
 
 #include "CCEUtil.h"
+#include "platform\CCFileSystemProtocol.h"
 
 // CCO4File
 CC_BEGIN_CALLS(CCO4File, CCObject)
@@ -13,121 +14,22 @@ CC_BEGIN_CALLS(CCO4File, CCObject)
 	CC_DEFINE_CALL(CCO4File, delete)
 CC_END_CALLS(CCO4File, CCObject)
 
-std::string vpath(std::string name)
-{
-	if(name.length()>1 && name[0]=='/') {
-		name = name.substr(1);
-	}
-	std::string path = CCFileUtils::sharedFileUtils()->getWritablePath();
-	return path+name;
-}
-
 CCValue CCO4File::CALLNAME(exists)(CCValueArray& params)
 {
-	std::string name = ccvpString(params,0);
-	name = vpath(name);
-	CCFileUtils* fs = CCFileUtils::sharedFileUtils();
-	return CCValue::booleanValue(fs->isFileExist(name));
+	std::string name = ccvpString(params,0);	
+	CCFileSystemProtocol* fs = CCFileSystemProtocol::sharedFileSystem();
+	return CCValue::booleanValue(fs->fileExists(kAppData, name.c_str()));
 }
 
 CCValue CCO4File::CALLNAME(load)(CCValueArray& params)
 {
 	std::string name = ccvpString(params,0);
-	name = vpath(name);
-	
-	int size = 0;
-	char* buf = NULL;
-	do {
-		// read the file from hardware
-		FILE *fp = fopen(name.c_str(), "rb");
-		CC_BREAK_IF(!fp);
-        
-		fseek(fp,0,SEEK_END);
-		size = ftell(fp);
-		fseek(fp,0,SEEK_SET);
-		buf = new char[size];
-		fread(buf,sizeof(char), size,fp);
-		fclose(fp);
-    } while (0);
+	CCFileSystemProtocol* fs = CCFileSystemProtocol::sharedFileSystem();
+	unsigned long size = 0;
+	unsigned char* buf = NULL;
+	buf = fs->fileRead(kAppData, name.c_str(), &size);	
     
     if (! buf)
-    {
-        std::string msg = "file.load(";
-        msg.append(name).append(") failed!");        
-        CCLOG("%s", msg.c_str());
-		return CCValue::stringValue("");
-    }
-    
-	std::string r(buf, size);
-	delete[] buf;
-	return CCValue::stringValue(r);
-}
-
-CCValue CCO4File::CALLNAME(save)(CCValueArray& params)
-{
-	std::string name = ccvpString(params,0);
-	std::string content = ccvpString(params, 1);
-	name = vpath(name);
-	
-	// read the file from hardware
-	FILE *fp = fopen(name.c_str(), "wb");
-	bool r = false;
-	if(fp) {
-		size_t wcount = fwrite(content.c_str(), content.length(),1, fp);
-		fclose(fp);	
-		r = true;
-	}
-
-	if(!r) {
-		std::string msg = "file.save(";
-        msg.append(name).append(") failed!");
-        CCLOG("%s", msg.c_str());
-	}
-	return CCValue::booleanValue(r);
-}
-
-CCValue CCO4File::CALLNAME(delete)(CCValueArray& params)
-{
-	std::string name = ccvpString(params,0);
-	name = vpath(name);
-	bool r = false;
-#if CC_TARGET_PLATFORM==CC_PLATFORM_WIN32
-	r = ::DeleteFileA(name.c_str())==TRUE;
-#endif
-	return CCValue::booleanValue(r);
-}
-
-/*
-#if CC_TARGET_PLATFORM==CC_PLATFORM_WIN32
-// CCO4VFSFile
-#include "../sqlite/SQLiteVFS.h"
-
-CC_BEGIN_CALLS(CCO4VFSFile, CCObject)
-	CC_DEFINE_CALL(CCO4VFSFile, exists)
-	CC_DEFINE_CALL(CCO4VFSFile, load)
-	CC_DEFINE_CALL(CCO4VFSFile, save)
-	CC_DEFINE_CALL(CCO4VFSFile, delete)
-CC_END_CALLS(CCO4VFSFile, CCObject)
-
-void CCO4VFSFile::init(SQLiteVFS* vfs)
-{
-	m_vfs = vfs;
-}
-
-CCValue CCO4VFSFile::CALLNAME(exists)(CCValueArray& params)
-{
-	std::string name = ccvpString(params,0);	
-	bool r = m_vfs->fileExists(name.c_str());
-	return CCValue::booleanValue(r);
-}
-
-CCValue CCO4VFSFile::CALLNAME(load)(CCValueArray& params)
-{
-	std::string name = ccvpString(params,0);
-		
-	int size = 0;
-	char* buf = (char*) m_vfs->fileRead(name.c_str(), &size);
-    if (!buf)
     {
         std::string msg = "file.load(";
         msg.append(name).append(") failed!");        
@@ -140,12 +42,15 @@ CCValue CCO4VFSFile::CALLNAME(load)(CCValueArray& params)
 	return CCValue::stringValue(r);
 }
 
-CCValue CCO4VFSFile::CALLNAME(save)(CCValueArray& params)
+CCValue CCO4File::CALLNAME(save)(CCValueArray& params)
 {
 	std::string name = ccvpString(params,0);
 	std::string content = ccvpString(params, 1);
 	
-	bool r = m_vfs->fileWrite(name.c_str(),(byte*) content.c_str(), content.length(), 0)>=0;
+	CCFileSystemProtocol* fs = CCFileSystemProtocol::sharedFileSystem();
+	unsigned long wcount = fs->fileWrite(kAppData, name.c_str(), (unsigned char*) content.c_str(), content.length());
+	
+	bool r = wcount!=content.length();	
 
 	if(!r) {
 		std::string msg = "file.save(";
@@ -155,11 +60,11 @@ CCValue CCO4VFSFile::CALLNAME(save)(CCValueArray& params)
 	return CCValue::booleanValue(r);
 }
 
-CCValue CCO4VFSFile::CALLNAME(delete)(CCValueArray& params)
+CCValue CCO4File::CALLNAME(delete)(CCValueArray& params)
 {
 	std::string name = ccvpString(params,0);
-	bool r = m_vfs->fileDelete(name.c_str(), false);
+	
+	CCFileSystemProtocol* fs = CCFileSystemProtocol::sharedFileSystem();
+	bool r = fs->fileDelete(kAppData, name.c_str());
 	return CCValue::booleanValue(r);
 }
-#endif
-*/
